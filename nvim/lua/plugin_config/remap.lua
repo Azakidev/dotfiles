0b075af2563eb98afdf096d4e35bcf82ff36fdff
Azakidev <zazaguichi@outlook.com>
2024-06-11 13:36:07 +0200

    Finish remaps for now

--- nvim/lua/plugin_config/remap.lua
+++ nvim/lua/plugin_config/remap.lua
@@ -10,9 +10,12 @@ vim.keymap.set('n', "<A-Left>", vim.cmd.BufferPrevious)
 vim.keymap.set({ 'n', 'i', 'v' }, "<C-z>", vim.cmd.undo)
 vim.keymap.set({ 'n', 'i', 'v' }, "<C-y>", vim.cmd.redo)
 
--- Write and quit
+-- Write, source and quit
 vim.keymap.set('n', "<C-s>", vim.cmd.w)
 vim.keymap.set('n', "<A-q>", vim.cmd.q)
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
 
 -- Search and replace
 vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
@@ -20,6 +23,7 @@ vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
@@ -27,6 +31,3 @@ if (vim.loop.os_uname().sysname ~= "Windows_NT") then
     vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 end
 
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end)
