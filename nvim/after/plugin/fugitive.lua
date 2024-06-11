5a2aef509556e1b9624fa3446a6f5a70d3cd742a
Azakidev <zazaguichi@outlook.com>
2024-06-11 16:41:16 +0200

    Fix fugitive

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,6 +1,6 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-local opts = {remap = false}
+local opts = { remap = false }
 
 vim.keymap.set("n", "<leader>p", function()
     vim.cmd.Git('push')
@@ -8,5 +8,5 @@ end, opts)
 
 -- rebase always
 vim.keymap.set("n", "<leader>P", function()
-    vim.cmd.Git({ 'pull', '--rebase' })
+    vim.cmd.Git('pull --rebase')
 end, opts)
