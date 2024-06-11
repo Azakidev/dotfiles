7f371918b8e06ddfb765d7dc239d0bf28cc9914d
Azakidev <zazaguichi@outlook.com>
2024-06-11 02:28:00 +0200

    More lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -9,16 +9,19 @@ require('mason').setup()
 
 require("mason-lspconfig").setup {
     ensure_installed = {
+        "lua_ls",
+
         "rust_analyzer",
+        "taplo",
+
         "html",
         "unocss",
         "tsserver",
-        "emmet_ls",
         "jsonls",
-        "taplo",
-        "lua_ls",
     },
 }
+
+require('lspconfig').taplo.setup({})
 
 -- Fix Undefined global 'vim'
 lsp.configure('lua_ls', {
@@ -34,6 +37,7 @@ lsp.configure('lua_ls', {
 require('luasnip.loaders.from_vscode').lazy_load()
 
 local cmp = require('cmp')
+local cmp_format = require('lsp-zero').cmp_format({ details = true })
 local cmp_action = require('lsp-zero').cmp_action()
 
 local cmp_mappings = cmp.mapping.preset.insert({
@@ -45,8 +49,9 @@ local cmp_mappings = cmp.mapping.preset.insert({
 
 cmp.setup({
     sources = {
-        {name = 'nvim_lsp'},
-        {name = 'luasnip'},
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+        { name = 'luasnip' },
     },
     window = {
         completion = cmp.config.window.bordered(),
@@ -58,14 +63,14 @@ cmp.setup({
             require('luasnip').lsp_expand(args.body)
         end,
     },
+    formatting = cmp_format,
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+    local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 end)
 
 lsp.setup()
--- nvim/lua/plugin_config/lazy.lua
+++ nvim/lua/plugin_config/lazy.lua
@@ -57,6 +57,7 @@ local plugins = {
 
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-nvim-lua',
     'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
     'mg979/vim-visual-multi',
