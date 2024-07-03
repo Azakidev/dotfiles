126f20066a14452b8d762ff4bc359da744445043
Azakidev <zazaguichi@outlook.com>
2024-07-03 02:46:57 +0200

    Use wrapping defauts

--- nvim/after/plugin/wrapping.lua
+++ nvim/after/plugin/wrapping.lua
@@ -1,13 +1,1 @@
-local opts = {
-    auto_set_mode_filetype_allowlist = {
-        "asciidoc",
-        "gitcommit",
-        "latex",
-        "markdown",
-        "rst",
-        "tex",
-        "text",
-    },
-}
-
-require("wrapping").setup(opts)
+require("wrapping").setup()
