d828eabb207e118e17cb75bf5d05dcf6d016f8cc
Azakidev <zazaguichi@outlook.com>
2024-11-13 14:00:32 +0100

    Clean up powershell

--- pwsh.ps1
+++ pwsh.ps1
@@ -2,25 +2,20 @@ $env:POSH_GIT_ENABLED=$true
 Import-Module posh-git
 
 # Functions
-function battery { Get-CimInstance -Class batterystatus -Namespace root/wmi }
+    # Sync with unison
 function syncart { Param($arg) unison -ignore "Name desktop.ini" -batch $arg C:\Users\zazag\Pictures\Art ssh://zazag@192.168.0.17/Pictures/Art }
 function syncm { Param($arg) unison -ignore "Name desktop.ini" -batch $arg C:\Users\zazag\Music ssh://zazag@192.168.0.17/Music }
-function syncobsidian { Param($arg) unison -ignore "Name desktop.ini" -batch $arg C:\Users\zazag\Documents\Obsidian ssh://zazag@192.168.0.17/Documents/Obsidian }
+function syncobsidian { Param($arg) unison -ignore "Name desktop.ini" -ignore "Name .stfolder" -batch $arg C:\Users\zazag\Documents\Obsidian ssh://zazag@192.168.0.17/Documents/Obsidian }
+    # Utilities
 function upd { Param($arg) winget update -r $arg }
 function ezals { Param($arg) eza --icons=always --no-quotes $arg }
 function explore { explorer . }
 function fetch { fastfetch --config $HOME\Projects\dotfiles\fastfetch\wconfig.jsonc }
-
-# Git aliases
-function status { git status }
-function commit { Param($arg) git commit $arg }
-function add    { Param($arg) git add $arg }
-
-Set-Alias gs status
-Set-Alias gic commit
-Set-Alias ga add
-
-# Komorebi
+    # Git aliases
+function gs { git status }
+function gic { Param($arg) git commit $arg }
+function ga    { Param($arg) git add $arg }
+    # Komorebi
 function restile {
     taskkill /f /im komorebi.exe ;
     taskkill /f /im komorebi-bar.exe ;
