75840226fbed2858886d5dfd9a0c0265857b7a54
Azakidev <zazaguichi@outlook.com>
2025-05-26 10:39:36 +0200

    More Powershell changes

--- pwsh.ps1
+++ pwsh.ps1
@@ -2,14 +2,9 @@ $env:POSH_GIT_ENABLED=$true
 Import-Module posh-git
 
 # Functions
-    # Sync with unison
-function syncart { Param($arg) unison -ignore "Name desktop.ini" -batch $arg C:\Users\zazag\Pictures\Art ssh://zazag@192.168.0.17/Pictures/Art }
-function syncm { Param($arg) unison -ignore "Name desktop.ini" -batch $arg C:\Users\zazag\Music ssh://zazag@192.168.0.17/Music }
-function syncobsidian { Param($arg) unison -ignore "Name desktop.ini" -ignore "Name .stfolder" -batch $arg C:\Users\zazag\Documents\Obsidian ssh://zazag@192.168.0.17/Documents/Obsidian }
     # Utilities
 function upd { Param($arg) winget update -r $arg }
 function ezals { Param($arg) eza --icons=always --no-quotes $arg }
-function explore { explorer . }
 function fetch { fastfetch --config $HOME\Projects\dotfiles\fastfetch\wconfig.jsonc }
     # Git aliases
 function gs   { git status }
@@ -24,7 +19,7 @@ function ga   {
     }
     git status;
 }
-
+    # The time saver of all time
 function so {
     Param($arg)
     if ($null -ne $arg) {
@@ -44,7 +39,6 @@ Set-Alias clr clear
 Set-Alias vim nvim
 Set-Alias ls ezals
 Set-Alias cat bat
-Set-Alias ex explore
 
 oh-my-posh init pwsh --config ~\omp.toml | Invoke-Expression
 
