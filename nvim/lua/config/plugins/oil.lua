6160909ef36ccd58ac8c94789c354bb9d953a81c
Azakidev <zazaguichi@outlook.com>
2025-12-11 15:37:11 +0100

    More nvim

--- nvim/lua/config/plugins/oil.lua
+++ nvim/lua/config/plugins/oil.lua
@@ -8,32 +8,35 @@ return {
             watch_for_changes = true,
             skip_confirm_for_simple_edits = true,
 
-            view_options = {
-                show_hidden = true,
-            },
-
             lsp_file_methods = {
                 enabled = true,
                 timeout_ms = 1000,
                 autosave_changes = true,
             },
+
+            view_options = {
+                show_hidden = true,
+            },
         },
 
         config = function()
             local oil = require("oil")
+
             oil.setup()
+
             vim.keymap.set({ 'n' }, "<leader>e", function()
                 if vim.bo.filetype == 'oil' then
-                    require("oil").close()
+                    vim.cmd([[w]])
+                    oil.close()
                 else
                     vim.cmd([[Oil --float]])
                 end
             end, { desc = "File navigation" })
-            vim.keymap.set({'n'}, "<leader>s", vim.cmd.w)
+            vim.keymap.set({ 'n' }, "<leader>s", vim.cmd.w)
         end,
 
         -- Optional dependencies
-        dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+        dependencies = { "nvim-tree/nvim-web-devicons" },
         -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
         lazy = false,
     },
