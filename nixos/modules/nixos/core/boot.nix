c1d5685ecab97934a0ff01b6483cd100b7147bcd
Azakidev <zazaguichi@outlook.com>
2026-06-01 14:45:09 +0200

    Bump nix kernel

--- nixos/modules/nixos/core/boot.nix
+++ nixos/modules/nixos/core/boot.nix
@@ -12,16 +12,5 @@
     boot.loader.systemd-boot.configurationLimit=10;
 
     # Kernel
-    boot.kernelPackages = pkgs.linuxPackagesFor (
-        pkgs.linux_latest.override {
-            argsOverride = rec {
-            version = "7.0.6";
-            modDirVersion = "7.0.6";
-            src = pkgs.fetchurl {
-                url = "mirror://kernel/linux/kernel/v7.x/linux-${version}.tar.xz";
-                sha256 = "08vm18wx6399phzgr3wz94yga3ab4fyca79445ygvbspm904996b";
-                };
-            };
-        }
-    );
+    boot.kernelPackages = pkgs.linuxPackages_latest;
 }
