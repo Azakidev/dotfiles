8d52d6824218f1d2c23bb860d707ffd349cd4118
Azakidev <zazaguichi@outlook.com>
2026-09-11 00:53:25 +0200

    NixOS boot prettify and silencing

--- nixos/modules/nixos/core/boot.nix
+++ nixos/modules/nixos/core/boot.nix
@@ -7,10 +7,47 @@
 
 {
     # Bootloader
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
-    boot.loader.systemd-boot.configurationLimit=10;
+    boot = {
+        loader = {
+            systemd-boot = {
+                enable = true;
+                configurationLimit=10;
+            };
 
-    # Kernel
-    boot.kernelPackages = pkgs.linuxPackages_latest;
+            efi.canTouchEfiVariables = true;
+        };
+
+
+        plymouth = {
+            enable = true;
+            theme = "spinner_alt";
+            themePackages = with pkgs; [
+                (adi1090x-plymouth-themes.override {
+                 selected_themes = [ "spinner_alt" ];
+                 })
+            ];
+        };
+
+        # Kernel
+        kernelPackages = pkgs.linuxPackages_latest;
+
+        kernelParams = lib.mkBefore [
+            "fbcon=nodefer"                 # Wipes the vendor logo earlier
+            "vt.global_cursor_default=0"    # Stops cursor blinking while booting
+            "quiet"                         # Less log messages
+            "systemd.show_status=auto"      # Only show systemd errors
+            "udev.log_level=3"              # Only show udev errors
+            "plymouth.use-simpledrm"        # Faster plymouth splash
+            "splash"                        # Show splash
+        ];
+
+        consoleLogLevel = 3;
+
+        initrd = {
+            verbose = false;                # Less stage1 messages
+            systemd.enable = true;          # Use systemd initrd
+        };
+
+        loader.timeout = 1;
+    };
 }
