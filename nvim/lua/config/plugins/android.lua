fce391f4e4ff4072e6761ea806b0db905bee778c
Azakidev <zazaguichi@outlook.com>
2026-09-13 14:12:10 +0200

    Kotlin and android nvim tweaks

--- nvim/lua/config/plugins/android.lua
+++ nvim/lua/config/plugins/android.lua
@@ -1,15 +1,31 @@
 return {
     {
+        "AlexandrosAlexiou/kotlin.nvim",
+        ft = { "kotlin" },
+        dependencies = {
+            "mason.nvim",
+            "oil.nvim",
+            "trouble.nvim",
+        },
+
+        config = function()
+            require("kotlin").setup {
+                inlay_hints = { enabled = true },
+                folding = { enabled = true },
+            }
+        end
+    },
+    {
         "rrxxyz/droid-nvim",
         config = function()
             require("droid").setup({
                 android = {
                     auto_launch_app = false,
-                    qt_qpa_platform = "xcb",
+                    qt_qpa_platform = "wayland",
                 }
             })
 
-            vim.keymap.set('n', '<F5>', ":DroidRun<CR>", { desc = "Build and run an android app"})
+            vim.keymap.set('n', '<F5>', ":DroidRun<CR>", { desc = "Build and run an android app" })
         end,
     },
 }
